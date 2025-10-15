3a2842f9b6461a29cefb1a8006e959fd4d84d8ba
Ganidu Sathishka <h.kaushi49@gmail.com>
2025-10-16 04:11:19 +0530

    Fixed Typo of Sidebar & Folder Directory

--- layout/sidebar.php
+++ layout/sidebar.php
@@ -24,7 +24,7 @@
           <li onclick="window.location.href='<?= $baseURL ?>delivery_management/delivery.php'">
             <i class="fas fa-truck"></i><span>Delivery Management</span>
           </li>
-          <li onclick="window.location.href='<?= $baseURL ?>suppliers_buyers_management/frontend/supplier.php'">
+          <li onclick="window.location.href='<?= $baseURL ?>suppliers_buyers/frontend/supplier.php'">
             <i class="fas fa-users"></i><span>Suppliers & Buyers</span>
           </li>
           <li onclick="window.location.href='<?= $baseURL ?>finance_management/Finance.php'">
